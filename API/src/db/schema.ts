4e9abf389242e0423b77a2a58ace2e4379e866cf
Evan Saleck <evan.saleck@gmail.com>
2026-01-17 02:07:17 +0100

    [ADD] DB

--- API/src/db/schema.ts
+++ API/src/db/schema.ts
@@ -1,7 +1,37 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
     userID: serial("user_id").primaryKey().notNull(),
     email: text("email").notNull(),
     username: text("username").notNull(),
+    password: text("password").notNull(),
+    names: text("name").notNull(),
+    lastname: text("lastname").notNull(),
+    weight: text("weight").notNull(),
+    height: text("height").notNull(),
+    birthdate: text("birthdate").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow()
+});
+
+export const meals = pgTable("meals", {
+    mealID: serial("meal_id").primaryKey().notNull(),
+    userID: serial("user_id").notNull(),
+    mealName: text("meal_name").notNull(),
+    recipe: text("recipe"),
+    calories: text("calories").notNull(),
+    protein: text("protein").notNull(),
+    carbs: text("carbs").notNull(),
+    fats: text("fats").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow()
+}); 
+
+export const summarycalories = pgTable("summarycalories", {
+    summaryID: serial("summary_id").primaryKey().notNull(),
+    userID: serial("user_id").notNull(),
+    date: timestamp("date").notNull(),
+    totalCalories: text("total_calories").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow()
 });
